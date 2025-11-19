7c83983c71d60bddf8367ac2f8ea2cd7bc5f3b2e
RussellKaniki8299 <russellmk8299@gmail.com>
2025-11-19 17:47:05 +0100

    Correction serveur WebRTC / avec Live & Chat & Files & update contact

--- handlers/notificationHandlers.js
+++ handlers/notificationHandlers.js
@@ -1,9 +1,12 @@
-module.exports.registerNotificationHandlers = function (io, socket, users) {
+module.exports = function registerNotificationHandlers(io, socket, users) {
   // Notifications envoyées par le front
   socket.on("new-notification", ({ toUserId, type, payload }) => {
-    if (toUserId) {
-      const socketId = users[toUserId];
-      if (socketId) io.to(socketId).emit("new-notification", { type, payload });
+    const socketId = users[toUserId];
+    if (socketId) {
+      io.to(socketId).emit("new-notification", { type, payload });
+      console.log(`[Notification] Envoyée à user ${toUserId} | type: ${type}`);
+    } else {
+      console.log(`[Notification] User ${toUserId} non connecté`);
     }
   });
 
@@ -11,9 +14,15 @@ module.exports.registerNotificationHandlers = function (io, socket, users) {
   socket.on("update-amis", (data) => {
     if (data?.toUserId) {
       const socketId = users[data.toUserId];
-      if (socketId) io.to(socketId).emit("update-amis");
+      if (socketId) {
+        io.to(socketId).emit("update-amis");
+        console.log(`[Amis] Update envoyé à user ${data.toUserId}`);
+      } else {
+        console.log(`[Amis] User ${data.toUserId} non connecté`);
+      }
     } else {
       io.emit("update-amis");
+      console.log(`[Amis] Update envoyé à tous les utilisateurs`);
     }
   });
 };
